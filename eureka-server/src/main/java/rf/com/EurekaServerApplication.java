d0cfb4fcbeb660e37144054583a90a4f22c1ae21
박상수 <sangsu9701@naver.com>
2023-04-14 16:01:22 +0900

    spring cloud research with elasticsearch rest api

--- eureka-server/src/main/java/rf/com/EurekaServerApplication.java
+++ eureka-server/src/main/java/rf/com/EurekaServerApplication.java
@@ -1,13 +1,15 @@
 package rf.com;
 
+import org.apache.http.annotation.Obsolete;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
